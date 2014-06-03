7c3cd6d3a8f90e596578acd50cfd46b8af9b2f45
Srikanth <srikanth.nutigattu@gmail.com>
2014-06-03 21:44:07 +0800

    fixed js issue

--- cordova_spike/platforms/ios/www/js/app.js
+++ cordova_spike/platforms/ios/www/js/app.js
@@ -1,13 +1,46 @@
 // We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
 (function () {
 
+
+    var HomeView = function (adapter, template, listItemTemplate) {
+
+        this.findByName = function() {
+            adapter.findByName($('.search-key').val()).done(function(employees) {
+                    $('.employee-list').html(listItemTemplate(employees));
+            });
+        };
+
+        this.initialize = function () {
+            // Define a div wrapper for the view. The div wrapper is used to attach events.
+            this.el = $('<div/>');
+            this.el.on('keyup', '.search-key', this.findByName);
+        };
+
+
+        this.render = function () {
+          this.el.html(template());
+            return this;
+        };
+
+        
+        
+        this.initialize();
+
+    };
+
+
+
+
+
     /* ---------------------------------- Local Variables ---------------------------------- */
     var homeTpl = Handlebars.compile($("#home-tpl").html());
     var employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
     var adapter = new MemoryAdapter();
+
     adapter.initialize().done(function () {
-        renderHomeView();
+        $('body').html(new HomeView(adapter, homeTpl, employeeLiTpl).render().el);
     });
+   
     /* --------------------------------- Event Registration -------------------------------- */
      document.addEventListener('deviceready', function () {
         if (navigator.notification) { // Override default HTML alert with native dialog
@@ -24,15 +57,5 @@
 
 
     /* ---------------------------------- Local Functions ---------------------------------- */
-    function findByName() {
-        adapter.findByName($('.search-key').val()).done(function (employees) {
-            $('.employee-list').html(employeeLiTpl(employees));
-        });
-    }
-
-    function renderHomeView() {
-        $('body').html(homeTpl());
-        $('.search-key').on('keyup', findByName);
-    }
 
 }());
